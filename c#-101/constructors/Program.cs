b51171f48945b278ee4dc72ba494250c93724627
Özlem Karabulut <05170000061@ogrenci.ege.edu.tr>
2023-09-20 22:51:08 +0300

    add constructors lesson's practice homework

---
+++ c#-101/constructors/Program.cs
@@ -0,0 +1,47 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        Employee employeeOne = new Employee("Özlem", "Karabulut", 123456, "Mobile Development");
+        employeeOne.GetEmployeeInfo();
+
+        Console.WriteLine("************");
+
+        Employee employeeTwo = new Employee("Güneş", "Solmaz");
+        employeeTwo.GetEmployeeInfo();
+
+    }
+
+    class Employee
+    {
+        public string Name;
+        public string LastName;
+        public int No;
+        public string Department;
+
+        public Employee(string name, string lastName, int no, string department)
+        {
+            this.Name = name;
+            this.LastName = lastName;
+            this.No = no;
+            this.Department = department;
+        }
+
+
+        public Employee(string name, string lastName)
+        {
+            this.Name = name;
+            this.LastName = lastName;
+        }
+
+        public void GetEmployeeInfo()
+        {
+            Console.WriteLine("Name of the employee: {0}", Name);
+            Console.WriteLine("Last Name of the employee: {0}", LastName);
+            Console.WriteLine("Id of the employee: {0}", No);
+            Console.WriteLine("Department of the employee: {0}", Department);
+        }
+    }
+}
